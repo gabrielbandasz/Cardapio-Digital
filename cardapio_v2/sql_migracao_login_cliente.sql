7f39109f87bb5fb05f49e295b5799de6ea9677a3
gabrielbandasz <164923457+gabrielbandasz@users.noreply.github.com>
2026-05-18 17:11:32 -0300

    Delete cardapio_v2 directory

--- cardapio_v2/sql_migracao_login_cliente.sql
+++
@@ -1,5 +0,0 @@
--- Migração: adicionar suporte a login de clientes
--- Execute este script no banco cardapio_digital existente
-
-ALTER TABLE `clientes`
-  ADD COLUMN IF NOT EXISTS `senha_hash` varchar(255) DEFAULT NULL AFTER `whatsapp`;
